11456c55fb9619c8558943b0fdf0975429396309
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2026-03-06 07:15:44 +0200

    Update PortectRoutes.js

--- src/middleware/ProtectRoutes/PortectRoutes.js
+++ src/middleware/ProtectRoutes/PortectRoutes.js
@@ -5,7 +5,7 @@
 const ProtectRoutes = async (req, res, next) => {
  
     if (!req.session || !req.session.loggedIn) {
-        req.flash("error", "Your session has expired or you are not logged in.");
+
         return res.redirect("/Login");
     }
     next();
